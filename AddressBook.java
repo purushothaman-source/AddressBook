ac6b4643b32519c2b33677a02ce6cd533244d724
purushothaman-source <purushoth99jun@gmail.com>
2020-12-10 21:52:21 +0530

    ADDED : new person using arraylist

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,11 @@
 package com.day0.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Contacts {
 	String firstName, lastName, address, city, state;
 	long zip, phoneNumber;
-	int id;
 
 	public Contacts( String firstName, String lastName, String address, String city, String state, long zip,
 			long phoneNumber) {
@@ -14,12 +17,32 @@ class Contacts {
 		this.zip = zip;
 		this.phoneNumber = phoneNumber;
 	}
+
+	@Override
+	public String toString() {
+		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
+	
 }
 //main class
 public class AddressBook {
      //main method
 	public static void main(String[] args) {
 		System.out.println("Welcome to AddressBook");
+		Scanner input=new Scanner(System.in);
+		String firstName, lastName, address, city, state;
+		long zip, phoneNumber;
+		firstName=input.next();
+		lastName=input.next();
+		address=input.next();
+		city=input.next();
+		state=input.next();
+		zip=input.nextLong();
+		phoneNumber=input.nextLong();
+		ArrayList<Contacts> arraylist =new ArrayList<>();
+		arraylist.add(new Contacts(firstName, lastName, address, city, state, zip, phoneNumber));
+		System.out.println(arraylist);
 	}
 
 }
