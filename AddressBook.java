6837b5807fe91ee72342b095b01e00cacbf7e150
purushothaman-source <purushoth99jun@gmail.com>
2020-12-10 21:21:15 +0530

    ADDED :welcome message

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package com.day0.addressbook;
+//main class
+public class AddressBook {
+     //main method
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook");
+	}
+
+}
