b51c2ebd9275815fbde4eb1a087c7425c84b4970
purushothaman-source <purushoth99jun@gmail.com>
2020-12-10 21:28:13 +0530

    ADDED : contact person

--- AddressBook.java
+++ AddressBook.java
@@ -1,4 +1,20 @@
 package com.day0.addressbook;
+class Contacts {
+	String firstName, lastName, address, city, state;
+	long zip, phoneNumber;
+	int id;
+
+	public Contacts( String firstName, String lastName, String address, String city, String state, long zip,
+			long phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+}
 //main class
 public class AddressBook {
      //main method
