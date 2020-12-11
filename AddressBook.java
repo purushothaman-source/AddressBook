e3c22bea2af47e8c7a2abcc25a351b370abfa90e
purushothaman-source <purushoth99jun@gmail.com>
2020-12-11 07:40:15 +0530

    ADDED :editing information

--- AddressBook.java
+++ AddressBook.java
@@ -1,13 +1,14 @@
 package com.day0.addressbook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 class Contacts {
 	String firstName, lastName, address, city, state;
 	long zip, phoneNumber;
 
-	public Contacts( String firstName, String lastName, String address, String city, String state, long zip,
+	public Contacts(String firstName, String lastName, String address, String city, String state, long zip,
 			long phoneNumber) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -23,26 +24,79 @@ class Contacts {
 		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
 				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
 	}
-	
+
 }
+
 //main class
 public class AddressBook {
-     //main method
+	// main method
+	static ArrayList<Contacts> arraylist = new ArrayList<>();
+	static Scanner input = new Scanner(System.in);
+	static HashMap<String, Contacts> hashmap = new HashMap<String, Contacts>();
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to AddressBook");
-		Scanner input=new Scanner(System.in);
-		String firstName, lastName, address, city, state;
-		long zip, phoneNumber;
-		firstName=input.next();
-		lastName=input.next();
-		address=input.next();
-		city=input.next();
-		state=input.next();
-		zip=input.nextLong();
-		phoneNumber=input.nextLong();
-		ArrayList<Contacts> arraylist =new ArrayList<>();
-		arraylist.add(new Contacts(firstName, lastName, address, city, state, zip, phoneNumber));
-		System.out.println(arraylist);
+		while(true) {
+		System.out.println("choose the operation which u want to perform");
+		System.out.println("1.ADD");
+		System.out.println("2.EDIT");
+		int num = input.nextInt();
+		switch (num) {
+		case 1:
+			add();
+			break;
+		case 2:
+			edit();
+			break;
+		default:
+			System.out.println("Invalid");
+		}
+		System.out.println(" press(1?0)");
+		System.out.println("1 for stop");
+		int number=input.nextInt();
+		if(number ==1)
+			break;
+	}
+		System.out.println(hashmap);
+	}
+
+	public static void add() {
+		System.out.println("enter firstname");
+		String firstName = input.next();
+		System.out.println("enter lastname");
+		String lastName = input.next();
+		System.out.println("enter address");
+		String address = input.next();
+		System.out.println("enter city");
+		String city = input.next();
+		System.out.println("enter state");
+		String state = input.next();
+		System.out.println("enter zip");
+		long zip = input.nextLong();
+		System.out.println("enter phone number");
+		long phoneNumber = input.nextLong();
+
+		hashmap.put(firstName, new Contacts(firstName, lastName, address, city, state, zip, phoneNumber));
+	}
+
+	static void edit() {
+		System.out.println("ENTER THE FIRSTNAME WHICH YOU WANT TO EDIT");
+		System.out.println("enter firstname");
+		String firstName = input.next();
+		System.out.println("enter lastname");
+		String lastName = input.next();
+		System.out.println("enter address");
+		String address = input.next();
+		System.out.println("enter city");
+		String city = input.next();
+		System.out.println("enter state");
+		String state = input.next();
+		System.out.println("enter zip");
+		long zip = input.nextLong();
+		System.out.println("enter phone number");
+		long phoneNumber = input.nextLong();
+		hashmap.replace(firstName, new Contacts(firstName, lastName, address, city, state, zip, phoneNumber));
+
 	}
 
 }
